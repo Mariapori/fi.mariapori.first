7e7c3e8c63706200b21782caf5dd7d7a2e0054cb
Topias Mariapori <topias@mariapori.fi>
2021-08-09 22:22:03 +0300

    bugikorjauksia ja tarkistuksia

--- src/fi/mariapori/first/BuyOP.java
+++ src/fi/mariapori/first/BuyOP.java
@@ -13,16 +13,20 @@ public class BuyOP implements CommandExecutor {
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
 		if(arg0 instanceof Player) {
 			Player pelaaja = (Player)arg0;
-			ItemStack timangit = new ItemStack(Material.DIAMOND);
-			timangit.setAmount(10);
-			if(pelaaja.getInventory().contains(timangit)) {
+			ItemStack timangit = new ItemStack(Material.DIAMOND,10);
+			if(pelaaja.getInventory().contains(Material.DIAMOND, 10)) {
+				if(pelaaja.isOp()) {
+					pelaaja.sendMessage("Olet jo operaattori!");
+					return true;
+				}
 				pelaaja.getInventory().removeItem(timangit);
 				pelaaja.setOp(true);
 				pelaaja.sendMessage("Ostit OP 10 timantilla!");
-				return true;
+			}else {
+				pelaaja.sendMessage("Sinulla ei ole 10 timanttia!");
 			}
 		}
-		return false;
+		return true;
 	}
 
 }
