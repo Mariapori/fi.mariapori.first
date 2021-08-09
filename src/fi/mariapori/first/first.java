e060d6ee728defae5d958f935b0c72816380ce46
Topias Mariapori <topias@mariapori.fi>
2021-08-09 15:09:18 +0300

    teleporttia

--- src/fi/mariapori/first/first.java
+++ src/fi/mariapori/first/first.java
@@ -5,6 +5,7 @@ public class first extends JavaPlugin {
     @Override
     public void onEnable() {
     	this.getCommand("dimang").setExecutor(new Dimang());
+    	this.getCommand("tele").setExecutor(new Teleport());
     }
     @Override
     public void onDisable() {
